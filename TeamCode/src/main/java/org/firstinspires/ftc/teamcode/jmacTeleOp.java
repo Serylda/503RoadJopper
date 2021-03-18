ca39cd34b7a34f145674effbf911e8a6d08bf52a
Rosetta <ericyzt@hotmail.com>
2021-03-18 10:23:00 -0500

    changes
    -Drive Constants
    -Fixed Jmac

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jmacTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jmacTeleOp.java
@@ -63,12 +63,12 @@ public class jmacTeleOp extends LinearOpMode {
         Vision vision = new Vision(this);
         currentState = IDLE;
 
+        /*
         mDrive.FlyWheel1.setVelocityPIDFCoefficients(1.25, 0.125, 0, 12.5);
         mDrive.FlyWheel2.setVelocityPIDFCoefficients(1.25,0.125, 0, 12.5);
         mDrive.FlyWheel1.setPositionPIDFCoefficients(5.0);
         mDrive.FlyWheel2.setPositionPIDFCoefficients(5.0);
-
-
+        */
 
         waitForStart();
 
