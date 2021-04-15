29f101240dcb510d1cbaf3120ca91139855ba103
Kookumba6969 <56769395+V1kRaMD@users.noreply.github.com>
2021-04-14 19:09:43 -0500

    switch sets up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DoALittleJopping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DoALittleJopping.java
@@ -10,6 +10,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Vision;
+
 @SuppressWarnings("ALL")
 @Autonomous(name="let me jop", group="auto")
 //@Disabled
@@ -28,7 +30,35 @@ public class DoALittleJopping extends LinearOpMode {
         PARK,           // We park
         IDLE            // Our bot will enter the IDLE state when done
     }
+
+    enum State4 {
+        TRAJECTORY_1,
+        TURN,
+        SHOOT,
+        TRAJ_2,
+        DROP,
+        TRAJ_3,
+        GRAB,
+        TRAJ_4,
+        DROP_2,
+        PARK,
+        IDLE
+    }
+
+    enum State1{
+        TRAJECTORY_1,
+        DROP,
+        SHOOT,
+        TRAJ_2,
+        GRAB,
+        TRAJ_3,
+        DROP_2,
+        IDLE
+    }
+
     State currentState = State.IDLE;
+    State currentState1 = State.IDLE;
+    State currentState4 = State.IDLE;
 
     Pose2d startPose = new Pose2d(-62, -45, Math.toRadians(0));
 
@@ -39,6 +69,7 @@ public class DoALittleJopping extends LinearOpMode {
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Vision vision = new Vision(this);
         drive.claw.setPosition(0);
 
         drive.setPoseEstimate(startPose);
@@ -75,116 +106,139 @@ public class DoALittleJopping extends LinearOpMode {
         waitForStart();
 
         drive.claw.setPosition(0);
+        drive.FlyWheel2.setVelocityPIDFCoefficients(1.622, 0.1622, 0, 16.22);
+        drive.FlyWheel1.setVelocityPIDFCoefficients(1.26, 0.126, 0, 12.6);
+        int ringCount = vision.ringCount('r');
+        telemetry.addData("Ring Count: ", ringCount);
+        telemetry.update();
 
         if (isStopRequested()) return;
 
-        currentState = State.TRAJECTORY_1;
-        drive.claw.setPosition(0);
-        drive.followTrajectoryAsync(wobbleDropOne);
+        switch (ringCount){
 
-        while (opModeIsActive() && !isStopRequested()) {
-            switch (currentState) {
-                case TRAJECTORY_1:
+            case 0:
+                currentState = State.TRAJECTORY_1;
+                drive.claw.setPosition(0);
+                drive.followTrajectoryAsync(wobbleDropOne);
 
-                    if(!drive.isBusy()){
-                        currentState = State.DROP;
-                        drive.waitTimer.reset();
-                        drive.wobbleDrop();
+                while (opModeIsActive() && !isStopRequested()) {
+                    switch (currentState) {
+                        case TRAJECTORY_1:
 
+                            if(!drive.isBusy()){
+                                currentState = State.DROP;
+                                drive.waitTimer.reset();
+                                drive.wobbleDrop();
+
+                            }
+                            break;
+
+                        case DROP:
+
+                            if (!drive.isBusy()){
+                                currentState = State.TURN;
+
+                                drive.turnAsync(turnAngle);
+                            }
+                            break;
+
+                        case TURN:
+
+                            if (!drive.isBusy()){
+                                currentState = State.SHOOT;
+                                drive.waitTimer.reset();
+                                drive.shoot();
+
+
+                            }
+                            break;
+
+                        case SHOOT:
+
+                            if (drive.waitTimer.milliseconds() > 1900){
+                                currentState = State.TRAJ_2;
+
+                                drive.followTrajectoryAsync(backUp);
+                                drive.followTrajectoryAsync(wobbleGrab);
+                            }
+                            break;
+
+                        case TRAJ_2:
+
+                            if (!drive.isBusy()){
+                                currentState = State.GRAB;
+                                drive.waitTimer.reset();
+                                drive.wobbleGrab();
+                            }
+                            break;
+
+                        case GRAB:
+                            if(!drive.isBusy()){
+                                currentState = State.TRAJ_3;
+
+
+
+                                drive.followTrajectoryAsync(wobbleDropTwo);
+                            }
+
+                        case TRAJ_3:
+                            if(!drive.isBusy()){
+                                currentState = State.DROP_2;
+                                drive.waitTimer.reset();
+                                drive.wobbleDrop2();
+
+
+                            }
+                        case DROP_2:
+
+                            if (!drive.isBusy()){
+                                currentState = State.PARK;
+
+                                drive.followTrajectoryAsync(park);
+                            }
+                            break;
+
+                        case PARK:
+
+                            if (!drive.isBusy()){
+                                currentState = State.IDLE;
+
+
+                            }
+                            break;
+                        case IDLE:
+
+                            break;
                     }
-                    break;
 
-                case DROP:
-
-                    if (!drive.isBusy()){
-                        currentState = State.TURN;
-
-                        drive.turnAsync(turnAngle);
-                    }
-                    break;
-
-                case TURN:
-
-                    if (!drive.isBusy()){
-                        currentState = State.SHOOT;
-                        drive.waitTimer.reset();
-                        drive.shoot();
-
-
-                    }
-                    break;
-
-                case SHOOT:
-
-                    if (drive.waitTimer.milliseconds() > 1900){
-                        currentState = State.TRAJ_2;
-
-                        drive.followTrajectoryAsync(backUp);
-                        drive.followTrajectoryAsync(wobbleGrab);
-                    }
-                    break;
-
-                case TRAJ_2:
-
-                    if (!drive.isBusy()){
-                        currentState = State.GRAB;
-                        drive.waitTimer.reset();
-                        drive.wobbleGrab();
-                    }
-                    break;
-
-                case GRAB:
-                    if(!drive.isBusy()){
-                        currentState = State.TRAJ_3;
+                    drive.update();
+                    Pose2d poseEstimate = drive.getPoseEstimate();
 
 
 
-                        drive.followTrajectoryAsync(wobbleDropTwo);
-                    }
-
-                case TRAJ_3:
-                    if(!drive.isBusy()){
-                        currentState = State.DROP_2;
-                        drive.waitTimer.reset();
-                        drive.wobbleDrop2();
+                    // Print pose to telemetry
+                    telemetry.addData("x", poseEstimate.getX());
+                    telemetry.addData("y", poseEstimate.getY());
+                    telemetry.addData("heading", poseEstimate.getHeading());
+                    telemetry.update();
 
 
-                    }
-                case DROP_2:
-
-                    if (!drive.isBusy()){
-                        currentState = State.PARK;
-
-                        drive.followTrajectoryAsync(park);
-                    }
-                    break;
-
-                case PARK:
-
-                    if (!drive.isBusy()){
-                        currentState = State.IDLE;
-
-
-                    }
-                    break;
-                case IDLE:
-
-                    break;
-            }
-
-            drive.update();
-            Pose2d poseEstimate = drive.getPoseEstimate();
+                }
+                break;
 
 
 
-            // Print pose to telemetry
-            telemetry.addData("x", poseEstimate.getX());
-            telemetry.addData("y", poseEstimate.getY());
-            telemetry.addData("heading", poseEstimate.getHeading());
-            telemetry.update();
+            case 1:
 
 
+                break;
+
+            case 4:
+
+                break;
         }
+
+
 
     }
 }
